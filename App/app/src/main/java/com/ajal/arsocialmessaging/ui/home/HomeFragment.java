5c92db735c9799351b65b932e569aa1eb2619765
Lucy Randewich <lucy@Lucys-MacBook-Air.local>
2021-10-26 14:44:05 +0100

    Working camera, need to change to homeFragment rather than MainActivity

--- App/app/src/main/java/com/ajal/arsocialmessaging/ui/home/HomeFragment.java
+++ App/app/src/main/java/com/ajal/arsocialmessaging/ui/home/HomeFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,11 +27,9 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
             }
         });
         return root;
