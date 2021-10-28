8b0170faef2b3e5470573f01efdad861d2fc79e3
alexelwood14 <alex.elwood14@outlook.com>
2021-10-28 21:51:41 +0100

    Added main layout for notifications

--- App/app/src/main/java/com/ajal/arsocialmessaging/ui/notifications/NotificationsFragment.java
+++ App/app/src/main/java/com/ajal/arsocialmessaging/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,10 +17,15 @@ import androidx.lifecycle.ViewModelProvider;
 import com.ajal.arsocialmessaging.R;
 import com.ajal.arsocialmessaging.databinding.FragmentNotificationsBinding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
+    private List<String> notifications;
+    private ListView listView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +35,12 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        notifications = Arrays.asList(getResources().getStringArray(R.array.notifications));
+        listView = root.findViewById(R.id.notifications_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, notifications);
+        listView.setAdapter(adapter);
+
+        /* Why does this section of code cause an error???
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -35,6 +48,7 @@ public class NotificationsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        */
         return root;
     }
 
