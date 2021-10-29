b940522f71337771f6bf5af09a30e27c9a42f800
alexelwood14 <alex.elwood14@outlook.com>
2021-10-29 13:07:18 +0100

    Removed unused imports

--- App/app/src/main/java/com/ajal/arsocialmessaging/ui/notifications/NotificationsFragment.java
+++ App/app/src/main/java/com/ajal/arsocialmessaging/ui/notifications/NotificationsFragment.java
@@ -1,29 +1,16 @@
 package com.ajal.arsocialmessaging.ui.notifications;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.ajal.arsocialmessaging.R;
 import com.ajal.arsocialmessaging.databinding.FragmentNotificationsBinding;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
