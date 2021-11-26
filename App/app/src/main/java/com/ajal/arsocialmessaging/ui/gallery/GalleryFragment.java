ef1c6854c0f70e95d85d1c68729a9cf3f776e8db
Joanne <yulin20028@hotmail.co.uk>
2021-11-26 17:43:14 +0000

    Simple Gallery

--- App/app/src/main/java/com/ajal/arsocialmessaging/ui/gallery/GalleryFragment.java
+++ App/app/src/main/java/com/ajal/arsocialmessaging/ui/gallery/GalleryFragment.java
@@ -1,61 +1,53 @@
 package com.ajal.arsocialmessaging.ui.gallery;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-
 
 import com.ajal.arsocialmessaging.R;
 import com.ajal.arsocialmessaging.databinding.FragmentGalleryBinding;
-import com.ajal.arsocialmessaging.databinding.FragmentHomeBinding;
 import com.bumptech.glide.Glide;
-import com.sangcomz.fishbun.FishBun;
-import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
     private static final String TAG = "SkyWrite";
     private List<File> images;
-    private ListView listView;
+    private GridLayout galleryView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        Fresco.initialize(this.getContext());
+
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+//        SimpleDraweeView posterImage = root.findViewById(R.id.posterImage);
+//        Uri imgURI = Uri.parse("https://media-cldnry.s-nbcnews.com/image/upload/t_fit-2000w,f_auto,q_auto:best/newscms/2021_13/3461005/210331-spongebob-episode-mb-1240.jpg");
+//        posterImage.setImageURI(imgURI);
 
         File dir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_DCIM) + "/SkyWrite");
@@ -63,30 +55,38 @@ public class GalleryFragment extends Fragment {
         if (dir.exists()) {
             images = Arrays.asList(dir.listFiles().clone());
 
-            listView = root.findViewById(R.id.gallery_list_view);
-            List<ImageView> imageViewList = new ArrayList<>();
+            galleryView = root.findViewById(R.id.gallery_view);
+            List<SimpleDraweeView> imageViewList = new ArrayList<>();
 
             if (images != null) {
+                int column = 0;
+                int row = 0;
+                galleryView.setColumnCount(3);
+
                 for (int i = 0; i < images.size(); i++) {
-                    // TODO: work here
                     File file = images.get(i);
 
-                    // Create a new Card View
-                    ImageView card = new ImageView(this.getContext());
-                    card.setLayoutParams(new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.WRAP_CONTENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT
-                    ));
+                    // Create a new Image View
+                    ImageView img = new ImageView(this.getContext());
+                    img.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                    img.setPadding(10, 10, 10, 10);
                     Glide.with(this)
                             .load(Uri.fromFile(file))
-                            .placeholder(R.drawable.ic_launcher_foreground)
+                            .placeholder(R.drawable.ic_launcher_foreground) // temp
+                            .apply(RequestOptions.centerCropTransform())
                             .thumbnail(0.5f)
-                            .into(card);
+                            .into(img);
 
-                    imageViewList.add(card);
+                    galleryView.addView(img);
+                    column++;
+                    if (column % 3 == 0) {
+                        row++;
+                        column = 0;
+                    }
+                    Log.d(TAG, "row="+row+"col="+column);
                 }
-                ArrayAdapter<ImageView> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_gallery_item, imageViewList);
-                listView.setAdapter(adapter);
+//                ArrayAdapter<SimpleDraweeView> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.list_content, imageViewList);
+//                galleryView.setAdapter(adapter);
             }
         }
 
