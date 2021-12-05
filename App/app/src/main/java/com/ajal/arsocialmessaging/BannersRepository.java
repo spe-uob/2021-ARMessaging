2a0ddfd76de051ea29f7738a48a62c99f41dbe1a
Lucy Randewich <lucy@Lucys-MacBook-Air.local>
2021-12-05 16:07:24 +0000

    moving server files to new folder

--- App/app/src/main/java/com/ajal/arsocialmessaging/BannersRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ajal.arsocialmessaging;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BannersRepository extends CrudRepository<Banner, Integer>{
-}
--- App/app/src/main/java/com/ajal/arsocialmessaging/MessagesRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ajal.arsocialmessaging;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessagesRepository extends CrudRepository<Message, Integer>{
-}
