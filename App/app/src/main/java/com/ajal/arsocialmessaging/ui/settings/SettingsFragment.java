4fe047b4d399351ce5361fdeaa69dd2b94600dac
JoanneLin168 <joannelin168@outlook.com>
2022-01-28 11:56:27 +0000

    Begun working on preferences.xml

--- App/app/src/main/java/com/ajal/arsocialmessaging/ui/settings/SettingsFragment.java
+++ App/app/src/main/java/com/ajal/arsocialmessaging/ui/settings/SettingsFragment.java
@@ -1,49 +1,15 @@
 package com.ajal.arsocialmessaging.ui.settings;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.ajal.arsocialmessaging.R;
-import com.ajal.arsocialmessaging.databinding.FragmentSettingsBinding;
 
-public class SettingsFragment extends Fragment {
-
-    private SettingsViewModel settingsViewModel;
-    private FragmentSettingsBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-
-        binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSettings;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-    }
-
+public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
-
 }
