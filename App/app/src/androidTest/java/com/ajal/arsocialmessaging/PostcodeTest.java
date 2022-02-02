491d400d17774b7ef8e5b4777b88268d3b4a6f16
JoanneLin168 <joannelin168@outlook.com>
2022-02-02 22:22:15 +0000

    Moved tests around
    Created a separate file for postcode tests

--- App/app/src/androidTest/java/com/ajal/arsocialmessaging/PostcodeTest.java
+++ App/app/src/androidTest/java/com/ajal/arsocialmessaging/PostcodeTest.java
@@ -1,14 +1,16 @@
 package com.ajal.arsocialmessaging;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.ajal.arsocialmessaging.util.PostcodeHelper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,12 +18,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.ajal.arsocialmessaging", appContext.getPackageName());
-    }
+public class PostcodeTest {
 
+    @Test
+    public void test_GetCorrectPostCode() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals(PostcodeHelper.getPostCode(appContext, 51.4559275, -2.6031669), "BS8 1UB");
+    }
 }
