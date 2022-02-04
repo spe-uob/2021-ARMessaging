3aa3d5006f4989951afd4bb24580e6fdbc7493b1
laurcty <lrandewich@gmail.com>
2022-02-04 16:38:12 +0000

    Removing bannerId column

--- DBServer/src/main/java/ajal/arsocialmessaging/DBServer/Banner.java
+++ DBServer/src/main/java/ajal/arsocialmessaging/DBServer/Banner.java
@@ -1,5 +1,6 @@
 package ajal.arsocialmessaging.DBServer;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.sql.Timestamp;
 import javax.persistence.*;
@@ -7,19 +8,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity @Table(name = "banners")
-public class Banner {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(columnDefinition = "serial") @Getter @Setter
-    Integer id;
-
+@IdClass(Banner.class)
+public class Banner implements Serializable {
+    
+    @Id
     @Column(name = "postcode") @Getter @Setter
     String postcode;
-
-    //@ManyToOne @JoinColumn(name = "message_id") @Getter @Setter
-    //Message message;
 
     @Column(name = "message_id") @Getter @Setter
     Integer message;
 
+    @Id
     @Column(name = "created_at") @Getter @Setter
     Timestamp timestamp;
 
