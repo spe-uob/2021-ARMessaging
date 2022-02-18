daf03f3db613cc33ebfeeb246f626f8bb14eea68
JoanneLin168 <joannelin168@outlook.com>
2022-02-18 13:53:28 +0000

    Fixed bug where app would show a black screen when installing app
    This is because the permissions would be checked for locations *before* the user can actually accept them. This has now been fixed

--- App/app/src/main/java/com/ajal/arsocialmessaging/MainActivity.java
+++ App/app/src/main/java/com/ajal/arsocialmessaging/MainActivity.java
@@ -41,13 +41,99 @@ public class MainActivity extends AppCompatActivity {
         if (!PermissionHelper.hasPermissions(this)) {
             PermissionHelper.requestPermissions(this);
         }
+        else {
+            ConnectivityHelper.getInstance().setMainActivity(this);
+            // Initiate the location updates request if location is available
+            Context ctx = this.getApplicationContext();
+            LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+            PostcodeHelper postcodeHelper = PostcodeHelper.getInstance();
+            // Permissions check
+            if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            lm.requestLocationUpdates(LocationManager.FUSED_PROVIDER, 5000, 10, postcodeHelper);
 
+            binding = ActivityMainBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+
+            // Preferences
+            SharedPreferences theme = getSharedPreferences(getString(R.string.theme_id), Context.MODE_PRIVATE);
+            SharedPreferences darkM = getSharedPreferences(getString(R.string.dark_mode), Context.MODE_PRIVATE);
+
+            // Dark mode
+            if (darkM.getString("darkMode", "On").equals("On")) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                setTheme(theme.getInt("themeID", R.style.FontSizeMedium));
+            }
+
+            theme.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                    setTheme(sharedPreferences.getInt(key, R.style.FontSizeMedium));
+                }
+            });
+
+            BottomNavigationView navView = findViewById(R.id.nav_view);
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.navigation_home,
+                    R.id.navigation_message,
+                    R.id.navigation_notifications,
+                    R.id.navigation_settings,
+                    R.id.navigation_gallery)
+                    .build();
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            NavigationUI.setupWithNavController(binding.navView, navController);
+        }
+
+        return;
+    }
+
+    /**
+     * If the viewPager is opened, pressing back will "close" it
+     * Otherwise, use super.onBackPressed()
+     */
+    @Override
+    public void onBackPressed() {
+        ViewPager viewPager = findViewById(R.id.viewPagerMain);
+        RecyclerView rv = findViewById(R.id.rv);
+        if (viewPager != null) { // it can be null when the Gallery fragment is not open
+            if (viewPager.getVisibility() == View.VISIBLE) {
+                rv.setVisibility(View.VISIBLE);
+                viewPager.setVisibility(View.INVISIBLE);
+            }
+            else {
+                super.onBackPressed();
+            }
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // if Permissions aren't granted despite requesting for them, open the device's Settings page
+        if (!PermissionHelper.hasPermissions(this)) {
+            PermissionHelper.requestPermissionsIfDenied(this);
+            return;
+        }
+
+        // Set up app once permissions have been granted
         ConnectivityHelper.getInstance().setMainActivity(this);
         // Initiate the location updates request if location is available
         Context ctx = this.getApplicationContext();
         LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
         PostcodeHelper postcodeHelper = PostcodeHelper.getInstance();
-        // Permissions check
+        // Permissions check - required for lm.requestLocationUpdates
         if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
@@ -89,28 +175,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
-
-    /**
-     * If the viewPager is opened, pressing back will "close" it
-     * Otherwise, use super.onBackPressed()
-     */
-    @Override
-    public void onBackPressed() {
-        ViewPager viewPager = findViewById(R.id.viewPagerMain);
-        RecyclerView rv = findViewById(R.id.rv);
-        if (viewPager != null) { // it can be null when the Gallery fragment is not open
-            if (viewPager.getVisibility() == View.VISIBLE) {
-                rv.setVisibility(View.VISIBLE);
-                viewPager.setVisibility(View.INVISIBLE);
-            }
-            else {
-                super.onBackPressed();
-            }
-        }
-        else {
-            super.onBackPressed();
-        }
     }
 
 }
