a449fed6da1da2acd8ecd3cf3101ae2e52ebe46a
laurcty <lrandewich@gmail.com>
2022-03-03 17:52:14 +0000

    implemented splash screen

--- App/app/src/main/java/com/ajal/arsocialmessaging/MainActivity.java
+++ App/app/src/main/java/com/ajal/arsocialmessaging/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -34,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+
+        // Handle the splash screen transition.
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
 
         // Check that SkyWrite has the correct permissions and if not, request them
