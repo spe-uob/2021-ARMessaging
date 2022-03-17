e7979b3bfa8c4700c0287789f8adc453943f34df
alexelwood14 <alex.elwood14@outlook.com>
2022-03-17 13:58:42 +0000

    Small fixes

--- DBServer/src/main/java/ajal/arsocialmessaging/DBServer/BannersRepository.java
+++ DBServer/src/main/java/ajal/arsocialmessaging/DBServer/BannersRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.sql.Timestamp;
 
 public interface BannersRepository extends CrudRepository<Banner, Integer>{
 
     @Modifying
     @Query("DELETE FROM Banner b WHERE b.postcode = :postcode AND b.timestamp = :timestamp")
-    long deleteByPostcodeAndTimestamp(@Param("postcode") String postcode, @Param("timestamp") Timestamp timestamp);
+    @Transactional
+    void deleteByPostcodeAndTimestamp(@Param("postcode") String postcode, @Param("timestamp") Timestamp timestamp);
 }
