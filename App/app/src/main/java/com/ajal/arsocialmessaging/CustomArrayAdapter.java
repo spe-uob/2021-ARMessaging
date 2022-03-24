fc39bc9b7923a1667edb0dedd71ea20c09cb115e
laurcty <lrandewich@gmail.com>
2022-03-24 13:55:05 +0000

    Created new arrayAdapter and row item files

---
+++ App/app/src/main/java/com/ajal/arsocialmessaging/CustomArrayAdapter.java
@@ -0,0 +1,42 @@
+package com.ajal.arsocialmessaging;
+
+import android.widget.ArrayAdapter;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomArrayAdapter extends ArrayAdapter {
+
+        private String[] countryNames;
+        private String[] capitalNames;
+        private Integer[] imageid;
+        private Activity context;
+
+        public CustomArrayAdapter(Activity context, String[] countryNames, String[] capitalNames, Integer[] imageid) {
+            super(context, R.layout.row_item, countryNames);
+            this.context = context;
+            this.countryNames = countryNames;
+            this.capitalNames = capitalNames;
+            this.imageid = imageid;
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row=convertView;
+            LayoutInflater inflater = context.getLayoutInflater();
+            if(convertView==null)
+                row = inflater.inflate(R.layout.row_item, null, true);
+            TextView textViewCountry = (TextView) row.findViewById(R.id.textViewCountry);
+            TextView textViewCapital = (TextView) row.findViewById(R.id.textViewCapital);
+            ImageView imageFlag = (ImageView) row.findViewById(R.id.imageViewFlag);
+
+            textViewCountry.setText(countryNames[position]);
+            textViewCapital.setText(capitalNames[position]);
+            imageFlag.setImageResource(imageid[position]);
+            return  row;
+        }
+}
